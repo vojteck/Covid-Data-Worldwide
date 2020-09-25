0e5a24db574e9b5945fc597e235faead9c61da15
vojteck <abracadabra@yahoo.com>
2020-09-25 23:13:28 +0200

    Updates

---
+++ precache-manifest.89908fd833c7581a6e08e5971517a2db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5445f21b787fd5ecd901cc95a0f71081",
+    "url": "/Covid-Data-Worldwide/index.html"
+  },
+  {
+    "revision": "eaea064b2eac158bcdb0",
+    "url": "/Covid-Data-Worldwide/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "8df4284f9d5ae081e888",
+    "url": "/Covid-Data-Worldwide/static/css/main.e78edac7.chunk.css"
+  },
+  {
+    "revision": "eaea064b2eac158bcdb0",
+    "url": "/Covid-Data-Worldwide/static/js/2.67c45b7c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Covid-Data-Worldwide/static/js/2.67c45b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df4284f9d5ae081e888",
+    "url": "/Covid-Data-Worldwide/static/js/main.7ecd2c56.chunk.js"
+  },
+  {
+    "revision": "4afc93f8c6a2158da3df",
+    "url": "/Covid-Data-Worldwide/static/js/runtime-main.de95a744.js"
+  }
+]);
