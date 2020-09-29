bef602e57fb71a251c517854f970e6e9044bfd6f
vojteck <urbinho1@gmail.com>
2020-09-29 14:48:36 +0200

    Routing fixed

--- src/app/routing/Main.router.js
+++ src/app/routing/Main.router.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { WorldData } from '../../components/CovidData/World/WorldData.component';
 import DataRouter from './Data.router';
 
 export default function MainRouter() {
     return(
         <main>
             <Switch>
-                <Route exact path="/" component={WorldData} />
                 <Route path="/data" component={DataRouter} />
-                <Route component={Error} />
             </Switch>
         </main>
       );
